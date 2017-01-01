0efa9929c46c4213c6ce3822b7406de078a1af3a
Ben Vogt <benjvogt@gmail.com>
2017-01-01 14:51:24 -0600

    Documenting Cell.ts

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ TypeScript implementation of a spreadsheet.
 ## TODO
 Things I should do.
 
-### Write documentation for Cell
-
 ### Write documentation for Sheet
 
 ### Write documentation for Matrix
+
+### Cell.error should not be of type any
+Errors should have specific types so we can have type safety when passing them.
--- src/Cell.ts
+++ src/Cell.ts
@@ -1,3 +1,7 @@
+/**
+ * Cell represents a cell in the spreadsheet. It contains a nullable formula, and a value, which is not nullable unless
+ * the parsing of the formula results in an error.
+ */
 class Cell {
   private formula: string;
   private value: string;
@@ -6,6 +10,11 @@ class Cell {
   private id: string;
   private row: number;
   private col: number;
+
+  /**
+   * Creates an empty cell with an id.
+   * @param id key of the cell in A1-format.
+   */
   constructor(id: string) {
     var key = parseKey(id);
 
@@ -17,6 +26,11 @@ class Cell {
     this.row = key.y;
     this.col = key.x;
   }
+
+  /**
+   * Update this cell's dependencies, where `dependencies` is a unique list of A1-format cell IDs.
+   * @param dependencies to merge with existing dependencies.
+   */
   updateDependencies(dependencies: Array<string>) {
     for (var index in dependencies) {
       if (this.dependencies.indexOf(dependencies[index]) === -1) {
@@ -24,42 +38,105 @@ class Cell {
       }
     }
   }
+
+  /**
+   * Return a list of dependencies in A1-format cell IDs, in no particular order, but likely in order of occurrence in
+   * formula.
+   * @returns {Array<string>} list of dependencies in A1-format
+   */
   getDependencies() : Array<string> {
     return this.dependencies;
   }
+
+  /**
+   * Return the zero-indexed column number of this cell.
+   * @returns {number} column
+   */
   getColumn() : number {
     return this.col;
   }
+
+  /**
+   * Return the zero-indexed row number of this cell.
+   * @returns {number} row
+   */
   getRow() : number {
     return this.row;
   }
+
+  /**
+   * Get the A1-format ID of this cell.
+   * @returns {string} cell ID
+   */
   getId() : string {
     return this.id;
   }
+
+  /**
+   * Set the formula of this cell.
+   * @param formula to set.
+   */
   setFormula(formula: string) {
     this.formula = formula;
   }
+
+  /**
+   * Get the formula of this cell if set. Defaults to null, so could return null.
+   * @returns {string} formula of this cell, if set. Nullable.
+   */
   getFormula() : string {
     return this.formula;
   }
+
+  /**
+   * Set the value of this cell. If this cell has a primitive value (does not contain a formula), it could be set to a
+   * value while the formula field is still null.
+   * @param value to set
+   */
   setValue(value: string) {
     this.value = value;
   }
+
+  /**
+   * Get the value of this cell. Since value could be null do to an error in the formula, this could return null.
+   * @returns {string}
+   */
   getValue() : string {
     return this.value;
   }
+
+  /**
+   * Set error for this cell. Usually in the case of a parse error when parsing the formula.
+   * @param error to set.
+   */
   setError(error: any) {
     this.error = error;
   }
+
+  /**
+   * Get the error for this cell. If the formula is parsed properly, or is null, this could be null.
+   * @returns {any} error to return, could be Null.
+   */
   getError() : any {
     return this.error;
   }
+
+  /**
+   * Get the renderable value for this cell, meaning the value that is the most meaningful to the user. If the error is
+   * null, we return the error. If we don't have an error, return the value.
+   * @returns {string} renderable string value to return.
+   */
   getRenderedValue() : string {
     if (this.error !== null) {
-      return this.value;
+      return this.value.toString();
     }
-    return this.error.toString()
+    return this.error.toString();
   }
+
+  /**
+   * Returns the human-readable string representation of this cell, omitting some obvious fields.
+   * @returns {string} to return
+   */
   toString() : string {
     return "id=" + this.id + ", value=" + this.value + ", formula=" + this.formula + ", error=" + this.error;
   }

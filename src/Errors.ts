f6ed5aad815517c683d72266de9c8c1cb0ce8c37
Ben Vogt <benjvogt@gmail.com>
2017-01-01 15:05:42 -0600

    Making Errors.get use object instead of filtering array

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,20 +1,27 @@
 var Errors = {
-  errors: [
-    {type: 'NULL', output: '#NULL'},
-    {type: 'DIV_ZERO', output: '#DIV/0!'},
-    {type: 'VALUE', output: '#VALUE!'},
-    {type: 'REF', output: '#REF!'},
-    {type: 'NAME', output: '#NAME?'},
-    {type: 'NUM', output: '#NUM!'},
-    {type: 'NOT_AVAILABLE', output: '#N/A!'},
-    {type: 'ERROR', output: '#ERROR'}
-  ],
+  errors: {
+    'NULL': '#NULL',
+    '#NULL': '#NULL',
+    'DIV_ZERO': '#DIV/0!',
+    '#DIV/0!': '#DIV/0!',
+    'VALUE': '#VALUE!',
+    '#VALUE!': '#VALUE!',
+    'REF': '#REF!',
+    '#REF!': '#REF!',
+    'NAME': '#NAME?',
+    '#NAME?': '#NAME?',
+    'NUM': '#NUM!',
+    '#NUM!': '#NUM!',
+    'NOT_AVAILABLE': '#N/A!',
+    '#N/A!': '#N/A!',
+    'ERROR': '#ERROR',
+    '#ERROR': '#ERROR'
+  },
   get: function (type) {
-    var error = Errors.errors.filter(function (item) {
-      return item.type === type || item.output === type;
-    })[0];
-
-    return error ? error.output : null;
+    if (type in Errors.errors) {
+      return Errors.errors[type];
+    }
+    return null;
   }
 };
 

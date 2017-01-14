ae29982b6adcae2386f0706d793b93e5b0bf3ed6
Ben Vogt <benjvogt@gmail.com>
2017-01-14 16:07:26 -0600

    More formulas as RawFormulas

--- src/RawFormulas.ts
+++ src/RawFormulas.ts
@@ -19,6 +19,42 @@ var AVERAGEA = Formula["AVERAGEA"];
 var AVERAGEIF = Formula["AVERAGEIF"];
 var BASE = Formula["BASE"];
 var BIN2DEC = Formula["BIN2DEC"];
+var BESSELI = Formula["BESSELI"];
+var BESSELJ = Formula["BESSELJ"];
+var BESSELK = Formula["BESSELK"];
+var BESSELY = Formula["BESSELY"];
+var BETADIST = Formula["BETADIST"];
+var BETAINV = Formula["BETAINV"];
+var BITAND = Formula["BITAND"];
+var BITLSHIFT = Formula["BITLSHIFT"];
+var BITOR = Formula["BITOR"];
+var BITRSHIFT = Formula["BITRSHIFT"];
+var BITXOR = Formula["BITXOR"];
+var BIN2HEX = Formula["BIN2HEX"];
+var BIN2OCT = Formula["BIN2OCT"];
+var DECIMAL = Formula["DECIMAL"];
+var CEILING = Formula["CEILING"];
+var CEILINGMATH = Formula["CEILINGMATH"];
+var CEILINGPRECISE = Formula["CEILINGPRECISE"];
+var CHAR = Formula["CHAR"];
+var CODE = Formula["CODE"];
+var COMBIN = Formula["COMBIN"];
+var COMBINA = Formula["COMBINA"];
+var COMPLEX = Formula["COMPLEX"];
+var CONCATENATE = Formula["CONCATENATE"];
+var CONVERT = Formula["CONVERT"];
+var CORREL = Formula["CORREL"];
+var COS = Formula["COS"];
+var PI = function () {
+  return Math.PI;
+};
+var COSH = Formula["COSH"];
+var COT = Formula["COT"];
+var COTH = Formula["COTH"];
+var COUNT = Formula["COUNT"];
+var COUNTA = Formula["COUNTA"];
+var COUNTIF = Formula["COUNTIF"];
+var COUNTIFS = Formula["COUNTIFS"];
 
 export {
   ABS,
@@ -37,5 +73,39 @@ export {
   AVERAGEA,
   AVERAGEIF,
   BASE,
-  BIN2DEC
+  BIN2DEC,
+  BESSELI,
+  BESSELJ,
+  BESSELK,
+  BESSELY,
+  BETADIST,
+  BETAINV,
+  BITAND,
+  BITLSHIFT,
+  BITOR,
+  BITRSHIFT,
+  BITXOR,
+  BIN2HEX,
+  BIN2OCT,
+  DECIMAL,
+  CEILING,
+  CEILINGMATH,
+  CEILINGPRECISE,
+  CHAR,
+  CODE,
+  COMBIN,
+  COMBINA,
+  COMPLEX,
+  CONCATENATE,
+  CONVERT,
+  CORREL,
+  COS,
+  PI,
+  COSH,
+  COT,
+  COTH,
+  COUNT,
+  COUNTA,
+  COUNTIF,
+  COUNTIFS
 }
--- tests/FormulasTest.ts
+++ tests/FormulasTest.ts
@@ -1,5 +1,8 @@
 import { ABS, ACOS, ACOSH, ACOTH, AND, ARABIC, ASIN, ASINH, ATAN, ATAN2, ATANH, AVEDEV, AVERAGE,
-  AVERAGEA, AVERAGEIF, BASE, BIN2DEC } from "../src/RawFormulas"
+    AVERAGEA, AVERAGEIF, BASE, BIN2DEC, BESSELI, BESSELJ, BESSELK, BESSELY, BETADIST, BETAINV,
+    BITAND, BITLSHIFT, BITOR, BITRSHIFT, BITXOR, BIN2HEX, BIN2OCT, DECIMAL, CEILING,
+    CEILINGMATH, CEILINGPRECISE, CHAR, CODE, COMBIN, COMBINA, COMPLEX, CONCATENATE, CONVERT,
+    CORREL, COS, PI, COSH, COT, COTH, COUNT, COUNTA, COUNTIF, COUNTIFS } from "../src/RawFormulas"
 import { assertEquals } from "./utils/Asserts"
 
 assertEquals(ABS(-10), 10);
@@ -37,3 +40,69 @@ assertEquals(AVERAGEIF([1, 5, 10], '>2'), 7.5);
 assertEquals(BASE(15, 2, 10), '0000001111');
 
 assertEquals(BIN2DEC(1010101010), -342);
+
+assertEquals(BESSELI(1, 2), 0.13574766658069928);
+
+assertEquals(BESSELJ(1, 2), 0.11490348499246938);
+
+assertEquals(BESSELK(1, 2), 1.6248388844172295);
+
+assertEquals(BESSELY(1, 2), -1.6506826133039476);
+
+assertEquals(BETADIST(2, 8, 10, true, 1, 3), 0.6854705810117458);
+
+assertEquals(BETAINV(0.6854705810117458, 8, 10, 1, 3), 1.9999999999999996);
+
+assertEquals(BITAND(42, 24), 8);
+
+assertEquals(BITLSHIFT(42, 24), 704643072);
+
+assertEquals(BITOR(42, 24), 58);
+
+assertEquals(BITRSHIFT(42, 2), 10);
+
+assertEquals(BITXOR(42, 24), 50);
+
+assertEquals(BIN2HEX(1010101010), "fffffffeaa");
+
+assertEquals(BIN2OCT(1010101010), "7777777252");
+
+assertEquals(DECIMAL(199.99999), 199);
+
+assertEquals(CEILING(22.22, 0.1), 22.3);
+
+assertEquals(CEILINGMATH(1001.112131), 1002);
+
+assertEquals(CEILINGPRECISE(1001.112131), 1002);
+
+assertEquals(CHAR(97), "a");
+
+assertEquals(CODE('a'), 97);
+
+assertEquals(COMBIN(4, 2), 6);
+
+assertEquals(COMBINA(4, 3), 20);
+
+assertEquals(COMPLEX(3, 4), '3+4i');
+
+assertEquals(CONCATENATE("hey", " ", "there"), "hey there");
+
+assertEquals(CONVERT(5.1, "mm", "m"), 0.0050999999999999995);
+
+assertEquals(CORREL([9, 5],[10, 4]), 1);
+
+assertEquals(COS(PI()), -1);
+
+assertEquals(COSH(PI()), 11.591953275521522);
+
+assertEquals(COT(30), -0.15611995216165922);
+
+assertEquals(COTH(2), 1.0373147207275482);
+
+assertEquals(COUNT([1, 5, 10]), 3);
+
+assertEquals(COUNTA(10, 10, 22), 3);
+
+assertEquals(COUNTIF([1, 5, 10], ">4"), 2);
+
+assertEquals(COUNTIFS([1, 5, 10], ">4", [1, 5, 10], ">4"), 2);

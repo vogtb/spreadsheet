fc9625c25e4b99305af2f4684bfbc4b5801ea6cd
Ben Vogt <benjvogt@gmail.com>
2017-05-01 18:01:25 -0500

    [Logical] Cleaning up file

--- src/Formulas/Logical.ts
+++ src/Formulas/Logical.ts
@@ -10,8 +10,10 @@ import {
 } from "../Errors";
 
 /**
- * Returns true if all of the provided arguments are logically true, and false if any of the provided arguments are logically false.
- * @param values At least one expression or reference to a cell containing an expression that represents some logical value, i.e. TRUE or FALSE, or an expression that can be coerced to a logical value.
+ * Returns true if all of the provided arguments are logically true, and false if any of the provided arguments are
+ * logically false.
+ * @param values At least one expression or reference to a cell containing an expression that represents some logical
+ * value, i.e. TRUE or FALSE, or an expression that can be coerced to a logical value.
  * @returns {boolean} if all values are logically true.
  * @constructor
  */
@@ -20,7 +22,8 @@ var AND = function (...values) {
   var result = true;
   for (var i = 0; i < values.length; i++) {
     if (typeof values[i] === "string") {
-      throw new ValueError("AND expects boolean values. But '" + values[i] + "' is a text and cannot be coerced to a boolean.")
+      throw new ValueError("AND expects boolean values. But '" + values[i]
+          + "' is a text and cannot be coerced to a boolean.")
     } else if (values[i] instanceof Array) {
       if (!AND.apply(this, values[i])) {
         result = false;
@@ -94,7 +97,8 @@ var NOT = function (...values) : boolean {
     if (X === "") {
       return true;
     }
-    throw new ValueError("Function NOT parameter 1 expects boolean values. But '" + X + "' is a text and cannot be coerced to a boolean.")
+    throw new ValueError("Function NOT parameter 1 expects boolean values. But '" + X
+        + "' is a text and cannot be coerced to a boolean.")
   }
   if (typeof(X) === "number") {
     return X === 0;
@@ -108,9 +112,10 @@ var NOT = function (...values) : boolean {
 };
 
 /**
- * Returns true if any of the provided arguments are logically true, and false if all of the provided arguments are logically false.
- * TODO: Should this allow the acceptance of functions that return true or false?
- * @param values An expression or reference to a cell containing an expression that represents some logical value, i.e. TRUE or FALSE, or an expression that can be coerced to a logical value.
+ * Returns true if any of the provided arguments are logically true, and false if all of the provided arguments are
+ * logically false.
+ * @param values An expression or reference to a cell containing an expression that represents some logical value, i.e.
+ * TRUE or FALSE, or an expression that can be coerced to a logical value.
  * @returns {boolean}
  * @constructor
  */
